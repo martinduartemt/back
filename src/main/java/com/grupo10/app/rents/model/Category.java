090f9d5d8fa34e1e3ae783de5cdc061df1bd3b66
martinduartemt <martin.duarte.mt@usa.edu.co>
2022-09-29 19:21:14 -0500

    seagregaron los modelos de los otros elemntos, mensaje, reservacion cliente y categoria cons sus repositorios

---
+++ src/main/java/com/grupo10/app/rents/model/Category.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupo10.app.rents.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Andres
+ */
+@Entity
+@Table(name="tb_")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="id")    
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private Quadbike quadbike;
+    
+
+ 
+
+    
+}
---
+++ src/main/java/com/grupo10/app/rents/model/Message.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupo10.app.rents.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Andres
+ */
+@Entity
+@Table(name="tb_message")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="id")    
+    private Integer idMessage;
+    @Column
+    private String messageText;
+    @Column
+    private Quadbike quadbike;
+    @Column
+    private Client client;
+    @Column
+    private Reservation reservation;
+    @Column
+    private String category;
+    
+
+ 
+
+    
+}
