419299fd779c1ce35f339d29b000fb058730bd65
martinduartemt <martin.duarte.mt@usa.edu.co>
2022-10-13 10:11:02 -0500

    update

--- src/main/java/com/grupo10/app/rents/controller/CategoryController.java
+++ src/main/java/com/grupo10/app/rents/controller/CategoryController.java
@@ -9,6 +9,7 @@ import com.grupo10.app.rents.service.CategoryService;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     @Autowired
